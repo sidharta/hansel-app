7df248740a7fc20764cfdce2df14f77ddb33167c
Sidharta Noleto <sidvelloso@gmail.com>
2016-04-25 15:29:48 -0300

    Listagem dos pontos no mapa

--- main/public/modules/crumbs/crumbs-list/crumbs.client.controller.js
+++ main/public/modules/crumbs/crumbs-list/crumbs.client.controller.js
@@ -2,13 +2,29 @@
     'use strict';
     var module = angular.module('admin');
 
-    module.controller('CrumbsController', function($scope) {
+    function updateMarkers(crumbs, map) {
+      angular.forEach(crumbs, function(o){
+        L.marker([o.lat, o.lng], {draggable: true}).addTo(map);
+      });
+    }
 
-        $scope.crumbs = [{code: 'ADS-1234'}, {code: 'JOI-9876'}];
+    module.controller('CrumbsController', function($scope, Restangular) {
+
+        $scope.crumbs = [];
 
         $scope.$on('$stateChangeSuccess', function() {
+            $scope.isLoading = true;
+
             L.mapbox.accessToken = 'pk.eyJ1Ijoic2lkaGFydGEiLCJhIjoiY2ltczg2OW1yMDFpNHZsbTR6MWs5ZHlwbSJ9.T5h2oS8vItUFM9__uoRvaA';
-            L.mapbox.map('map-one', 'mapbox.streets').setView([-19.8929, -49.0252], 8);
+            var map = L.mapbox.map('map-one', 'mapbox.streets').setView([-19.8929, -49.0252], 8);
+
+            Restangular.all('crumbs').getList().then(function(crumbs) {
+                $scope.crumbs = crumbs;
+
+                updateMarkers(crumbs, map);
+            }).finally(function() {
+                $scope.isLoading = false;
+            });
         });
     });
 }());
