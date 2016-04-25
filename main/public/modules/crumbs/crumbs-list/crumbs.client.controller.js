4b7fecb48d8e9a590eaecd41acb016243ea8ab89
Sidharta Noleto <sidvelloso@gmail.com>
2016-04-25 11:17:33 -0300

    Commit da tela de mapa

--- main/public/modules/crumbs/crumbs-list/crumbs.client.controller.js
+++ main/public/modules/crumbs/crumbs-list/crumbs.client.controller.js
@@ -2,40 +2,13 @@
     'use strict';
     var module = angular.module('admin');
 
-    module.controller('CrumbsController', function($scope, Restangular) {
+    module.controller('CrumbsController', function($scope) {
 
-        var ctrl = this;
-        var nextCursor = '';
-        var more = true;
         $scope.crumbs = [{code: 'ADS-1234'}, {code: 'JOI-9876'}];
-        $scope.center = {lat: 51.505,lng: -0.09,zoom: 4};
 
-        // ctrl.getUsers = function() {
-        //     if (!more) {
-        //         return;
-        //     }
-        //     $scope.isLoading = true;
-        //     Restangular.all('users').getList({cursor: nextCursor, filter: $scope.filter}).then(function(users) {
-        //         $scope.users = $scope.users.concat(users);
-        //         nextCursor = users.meta.nextCursor;
-        //         more = users.meta.more;
-        //         $scope.totalCount = users.meta.totalCount;
-        //     }).finally(function() {
-        //         $scope.isLoading = false;
-        //     });
-        // };
-        //
-        // ctrl.getUsers();
-        //
-        // // This is fired when user scrolled to bottom
-        // $scope.$on('mainContentScrolled', function() {
-        //     ctrl.getUsers();
-        // });
-
-        $scope.$on('$stateChangeSuccess', function(){
-          L.mapbox.accessToken = 'pk.eyJ1Ijoic2lkaGFydGEiLCJhIjoiY2ltczg2OW1yMDFpNHZsbTR6MWs5ZHlwbSJ9.T5h2oS8vItUFM9__uoRvaA';
-          L.mapbox.map('map-one', 'mapbox.streets').setView([-19.8929,-49.0252], 8);
+        $scope.$on('$stateChangeSuccess', function() {
+            L.mapbox.accessToken = 'pk.eyJ1Ijoic2lkaGFydGEiLCJhIjoiY2ltczg2OW1yMDFpNHZsbTR6MWs5ZHlwbSJ9.T5h2oS8vItUFM9__uoRvaA';
+            L.mapbox.map('map-one', 'mapbox.streets').setView([-19.8929, -49.0252], 8);
         });
-
     });
 }());
